d301120dfc393df4c55a5cbb8f51539ae1e1d491
BuildTools <unconfigured@null.spigotmc.org>
2020-11-16 18:43:28 -0600

    More ui changes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ import './App.css';
 const axios = require('axios')
 
 
+function Loader(props) {
+	if(props.visible) {
+		console.log("mstockm")
+		return (<div class="loadcontainer"><div class="loader"></div></div>)
+	} else {
+		console.log("nm")
+		return (<div class="loadcontainer"></div>)
+	}
+}
+
 function NewlineText(props) {
-  
+
+  if(props.text.split('\n').length < 2) {
+  	return <b>No users match this roster ID</b>
+  }
   return props.text.split('\n').slice(0, -1).map(str => {
   	const color = str.charAt(1);
+
 
 	  if(color === "X") {
 	  	return (<p class="pred"> <span class="dotr"></span>{str.substring(3,str.length)}</p>);
@@ -26,7 +40,7 @@ function NewlineText(props) {
 class UserRoster extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {stateText: "No state", rid: ""}
+    this.state = {stateText: "", rid: "", goodDataLoaded:false, curDataLoaded:""}
     this.inputRef = React.createRef();
 
   }
@@ -36,13 +50,15 @@ class UserRoster extends React.Component {
 		setInterval(function(){ 
 	  		thisObject.getOnlineState();
 		}, 1000);
+		setInterval(function(){ 
+	  		thisObject.setNewRID();
+		}, 50);
 	}
 
 
 	setNewRID = () => {
 		document.getElementById('root')
-		this.setState({stateText: this.state.stateText, rid: this.inputRef.current.value})
-
+		this.setState({rid:this.inputRef.current.value, goodDataLoaded:this.inputRef.current.value === this.state.curDataLoaded})
 	}
 
   getOnlineState = () => {
@@ -53,7 +69,7 @@ class UserRoster extends React.Component {
       	for(var i = 0; i < Object.keys(res.data).length; i++) {
 	        var entry = res.data[Object.keys(res.data)[i]];
 	        var rosterid = this.state.rid;
-
+			this.setState({curDataLoaded: rosterid})
 
 	        if(entry.roster === rosterid) {
 	        
@@ -97,11 +113,12 @@ class UserRoster extends React.Component {
   render() {
 
   	return (<div class="login-page">
-	  <p class="TitleText">CopBot Web Dashboard / Roster code {this.props.rosterid}</p>
+
+	  <p class="TitleText">CopBot Web Dashboard / Roster / {this.state.rid}</p>
 	  <input ref={this.inputRef} type="text" id="inputfield" placeholder="Roster name"></input>
-	  <input type="button" id="loginbutton" value="Apply" onClick={this.setNewRID}></input>
 	  <div class="form">
-	  	<NewlineText text={this.state.stateText} />
+	  <NewlineText text={this.state.stateText} />
+	  <Loader visible={!this.state.goodDataLoaded ? 1 : 0} />
 	  </div>
 	</div>);
   }
